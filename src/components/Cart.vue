edf4bbc3fa42557d6cdb39eae2d15c76666ffb93
NevineMGouda <Nevine.Gouda.9578@student.uu.se>
2018-12-20 19:40:54 +0100

    Persist the cart, in case of page refreshing.

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,13 +1,6 @@
 <template>
-    <div> Cart
+    <div>
         <h1> Your Cart items</h1>
-
-        <!--<div>{{ordertocart}}</div>-->
-        <!--<ul>-->
-            <!--<li v-for="item in ordertocart" :key="item">-->
-                <!--{{item.order.ingredients[0].ingredient_en}} - {{item.order.price}}-->
-            <!--</li>-->
-        <!--</ul>-->
 
         <div v-for="(item) in ordertocart" :key="item">
             {{"Burger " +item.id}} : {{item.data.order}}
@@ -29,13 +22,24 @@
             this.$store.state.socket.on('addItem2', function (data) {
                 var id =this.ordertocart.length;
                 this.ordertocart.push({id ,data});
-                console.log("ana honaaaa fi al cart");
-                console.log(data);
-                // if (typeof data.ingredients !== 'undefined') {
-                //     this.ingredients = data.ingredients;
-                // }
             }.bind(this));
+        },
+        mounted() {
+            console.log('App mounted/refreshed!');
+            // Get stored cart when mounting, refreshing. (persisting the data when the page is refreshed)
+            if (localStorage.getItem('ordertocart')) this.ordertocart = JSON.parse(localStorage.getItem('ordertocart'));
+        },
+        watch: {
+            ordertocart: {
+                handler() {
+                    console.log('ordertocart changed!');
+                    // Storre the cart to localStorage(browser's session) when the cart is updated.
+                    localStorage.setItem('ordertocart', JSON.stringify(this.ordertocart));
+                },
+                deep: true,
+            },
         }
+
 
     }
 </script>
