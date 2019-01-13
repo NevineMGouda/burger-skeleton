50dc88a6ce0da7e121310e2c8a7a24403baf0ed6
NevineMGouda <Nevine.Gouda.9578@student.uu.se>
2019-01-13 23:08:20 +0100

    Show Ingredients in separate lines in OrderItem

--- src/components/OrderItem.vue
+++ src/components/OrderItem.vue
@@ -1,12 +1,11 @@
 <template>
 	<div class="row">
-		<!--<div class="col-md-3 align-center ">{{orderId}}</div>-->
-		<!--<div class="col-md-4 align-center ">-->
-			<!--<span v-if="eatIn === 1 ">{{uiLabels.eatIn}}</span> <span v-if="eatIn === 0 ">{{uiLabels.takeAway}}</span>-->
-		<!--</div>-->
 		<div class="col-md-12">
-			{{order.order.ingredients.map(item=>item["ingredient_"+ lang]).join(", ") }}
+			<div v-for="item in order.order.ingredients">
+				{{item["ingredient_"+ lang]}}
+			</div>
 		</div>
+
 	</div>
 </template>
 <script>
